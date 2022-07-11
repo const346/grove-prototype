30d9953dd9c0111b67afdea173579286fb9f692b
const346 <const346@gmail.com>
2022-07-12 00:47:59 +0300

    Restored logic of a simple bot

--- Assets/Glove/Scripts/Character/BotController.cs
+++ Assets/Glove/Scripts/Character/BotController.cs
@@ -4,60 +4,63 @@ using UnityEngine;
 
 public class BotController : MonoBehaviour
 {
-    //private GravityTarget gravityObject;
-    //private Character controller;
-    //private bool actionBreak;
+    private GravityTarget gravityObject;
+    private Character controller;
+    private bool actionBreak;
 
-    //private void Awake()
-    //{
-    //    controller = GetComponent<Character>();
-    //    gravityObject = GetComponent<GravityTarget>();
-    //}
+    private void Awake()
+    {
+        controller = GetComponent<Character>();
+        gravityObject = GetComponent<GravityTarget>();
+    }
 
-    //private void OnEnable()
-    //{
-    //    StopAllCoroutines();
-    //    StartCoroutine(Decision());
-    //}
+    private void OnEnable()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Decision());
+    }
 
-    //private IEnumerator Decision()
-    //{
-    //    while (enabled)
-    //    {
-    //        var delay = Random.Range(0, 3);
-    //        yield return new WaitForSeconds(delay);
+    private IEnumerator Decision()
+    {
+        while (enabled)
+        {
+            var delay = Random.Range(0, 3);
+            yield return new WaitForSeconds(delay);
 
-    //        controller.Stand();
+            controller.Move(Vector3.zero);
 
-    //        if (Random.Range(0, 2) == 0)
-    //        {
-    //            var angle = Random.Range(0, 360);
-    //            yield return Move(angle);
-    //        }
-    //    }
-    //}
+            if (Random.Range(0, 2) == 0)
+            {
+                var angle = Random.Range(0, 360);
+                yield return Move(angle);
+            }
+        }
+    }
 
-    //private IEnumerator Move(float angle)
-    //{
-    //    var count = Random.Range(0, 3100);
-    //    for (var i = 0; i < count; i++)
-    //    {
-    //        if (actionBreak)
-    //        {
-    //            actionBreak = false;
-    //            yield break;
-    //        }
+    private IEnumerator Move(float angle)
+    {
+        var count = Random.Range(0, 3100);
 
-    //        controller.Move(angle);
-    //        yield return new WaitForFixedUpdate();
-    //    }
-    //}
+        controller.Look(0, angle);
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (Vector3.Angle(collision.contacts[0].normal, gravityObject.GetUp()) > 45)
-    //    {
-    //        actionBreak = true;
-    //    }
-    //}
+        for (var i = 0; i < count; i++)
+        {
+            if (actionBreak)
+            {
+                actionBreak = false;
+                yield break;
+            }
+
+            controller.Move(Vector3.forward);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Vector3.Angle(collision.contacts[0].normal, gravityObject.GetUp()) > 45)
+        {
+            actionBreak = true;
+        }
+    }
 }
